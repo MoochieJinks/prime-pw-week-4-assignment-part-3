bbbc057c3e90909373b62b3498032070170ec63a
Harrison Browning <moochiejinks@harrisons-mbp.lan>
2022-04-05 08:21:38 -0500

    updated assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,54 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// ### Required Features
+// Update the `cart.js` file to do the following:
+
+// - Create a global variable named `basket` and set it to an empty array.
+let basket = [];
+
+// - Create a function called `addItem`. It should:
+//   - take an input parameter for a string `item`
+//   - add the new item to the global array `basket`. 
+//   - return `true` indicating the item was added
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+// - Create a function called `listItems`. It should:
+//   - loop over the items in the `basket` array
+//   - console.log each individual item on a new line
+
+addItem('avacados');
+addItem('bananas');
+addItem('grapes');
+
+
+
+function listItems(){
+    for (const item of basket) {
+        console.log(item)
+    }
+}
+listItems();
+// - Create a function called `empty`. It should:
+//   - reset the `basket` to an empty array
+function empty(){
+    basket = [];
+}
+empty();
+console.log(`Basket is now ${basket}`);
+
+// > __IMPORTANT__
+// > Make sure that you are writing code *in the file* to test every function that you write!
+
+// For example, to test `addItem`:
+// ```
+// console.log(`Basket is ${basket}`);
+// console.log('Adding apples (expect true)', addItem('apples'));
+// console.log(`Basket is now ${basket}`);
